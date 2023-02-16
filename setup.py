b418c371cf0f00b45218798400b57f36672f6924
godmak2022@gmail.com <godmak2022@gmail.com>
2023-02-16 23:45:56 +0300

    Added setup file for python 3.4>3.9

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="max_min_value",
+    version="1.0",
+    description="A Python package for finding the maximum and minimum values in a 2-D Matrix.",
+    author="Godfrey-Ndungu",
+    author_email="godmak2022@gmail.com",
+    url="https://github.com/Godfrey-Ndungu/max-min-matrix",
+    packages=["max_min_value"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
